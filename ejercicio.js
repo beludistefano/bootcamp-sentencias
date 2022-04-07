4b56415e7c48635b830d3b7536a8dcbce84c9976
beludistefano <70746557+beludistefano@users.noreply.github.com>
2022-04-07 20:11:45 -0300

    Create ejercicio.js

---
+++ ejercicio.js
@@ -0,0 +1,38 @@
+if (numberoIf > 0){
+  console.log("El numero es mayor a cero")
+}else if(numberoIf < 0){
+  console.log("El numero es menor a cero")
+]else{
+  console.log("El numero es cero")
+}
+
+while(numeroWhile < 3){
+  numeroWhile++;
+  console.log(numeroWhile)
+}
+
+do {
+  console.log(numeroDo);
+  numeroDo ++;
+} while (numeroDo < 3);
+
+for(numeroFor = 0; numeroFor =< 3; numeroFor++){
+  console.log(numeroFor)
+}
+  
+switch (estacion) {
+  case 'Verano':
+    console.log('Estamos en verano');
+    break;
+  case 'Otoño':
+    console.log('Estamos en verano');
+    break;
+  case 'Invierno':
+    console.log('Estamos en verano');
+    break;
+  case 'Primavera':
+    console.log('Estamos en verano');
+    break;
+  default:
+    console.log('No es una estacion');
+}  
